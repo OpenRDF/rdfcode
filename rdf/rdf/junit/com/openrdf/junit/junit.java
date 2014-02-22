347ab29eda46eb172babf973c3efde680ffe54b1
Hawk <Hawk@Tiger>
2014-02-22 15:00:04 +0800

    test

---
+++ rdf/rdf/junit/com/openrdf/junit/junit.java
@@ -0,0 +1,21 @@
+package com.openrdf.junit;
+
+import java.io.PrintStream;
+
+import org.junit.internal.JUnitSystem;
+
+public class junit implements JUnitSystem {
+
+	@Override
+	public void exit(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public PrintStream out() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
